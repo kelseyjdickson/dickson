65158d433f6656c6c6e77776d2239f889733546d
kelseyjdickson <kelseymedved@gmail.com>
2020-12-07 16:58:22 -0500

    added sass to file

---
+++ src/components/Main/Main.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+
+import './Styles.scss';
+
+
+const Main = () => {
+
+    return (
+        <>
+       <h1 className="poop">Main Component</h1>
+
+        </>
+    )
+}
+
+export default Main
