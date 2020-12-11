6cd3f2485f5eefb1923bcac164dddea811530b12
kelseyjdickson <kelseymedved@gmail.com>
2020-12-11 13:51:07 -0500

    added photos file and folder

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import kelsey from './images/me.png'
+
+import '../Footer/Styles.scss'
 
 
 const Footer = () => {
     return(
-        <h1>Footer</h1>
+        <>
+        <h1 className="footer">Footer</h1>
+        <img src={kelsey} alt="picture of kelsey"/>
+        </>
     )
 }
 
