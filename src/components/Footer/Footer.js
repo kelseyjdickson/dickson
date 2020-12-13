368c32b75f758009e94453d557e4c820966fe8f8
kelseyjdickson <kelseymedved@gmail.com>
2020-12-13 16:23:01 -0500

    moved icons to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,7 +10,21 @@ const Footer = () => {
             <div className="footer">
                 <h1 className="footer__about">About</h1>
                 <img className="footer__image"src={kelsey} alt="picture of kelsey" />
-                <p>Developer in NYC with passion for front-end design and debugging</p>
+                <p className="footer__info">Developer in NYC with passion for front-end design and debugging</p>
+                <div className="icon-container">
+                    <a href="https://twitter.com/KelseyD485">
+                        <i class="fab fa-twitter-square fa-2x"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/feed/">
+                        <i className="link" class="fab fa-linkedin fa-2x"></i>
+                    </a>
+                    <a href="https://kelseyjdickson.medium.com/">
+                        <i class="fab fa-medium fa-2x"></i>
+                    </a>
+                    <a href="">
+                    <i class="fas fa-envelope-square fa-2x"></i>
+                    </a>
+                </div>
                 
             </div>
         </>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -12,17 +12,7 @@ const Main = (props) => {
         <>
             <div className="top">
                 <h1>Kelsey Dickson</h1>
-                <div className="icon-container">
-                    <a href="https://twitter.com/KelseyD485">
-                        <i class="fab fa-twitter-square fa-2x"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/feed/">
-                        <i className="link" class="fab fa-linkedin fa-2x"></i>
-                    </a>
-                    <a href="https://kelseyjdickson.medium.com/">
-                        <i class="fab fa-medium fa-2x"></i>
-                    </a>
-                </div>
+              
 
             </div>
 
