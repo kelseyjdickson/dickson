c9e15e5ccc31bcc241fe3cab4a0d16d551905f02
kelseyjdickson <kelseymedved@gmail.com>
2020-12-13 15:37:00 -0500

    href for twitter link

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,23 @@
 import React from "react"
-import twitter from './src/images/twit.png'
+import twitter from "./twit.png"
 
 
 
- import './Styles.scss';
+import './Styles.scss';
 
 
 const Main = (props) => {
 
     return (
         <>
-        <div className="top">
-            <h1>Kelsey Dickson</h1>
-            <img src={twitter}alt="twitter-logo"/>
-        </div>
-   
+            <div className="top">
+                <h1>Kelsey Dickson</h1>
+                <a href="https://twitter.com/KelseyD485">
+                    <img className="twitter" src={twitter} alt="twitter-logo" />
+                </a>
+
+            </div>
+
         </>
     )
 }
